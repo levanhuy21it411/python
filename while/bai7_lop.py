dcc189e81dec158e20da8fdf40c1725a948528fe
huybmt <levanhuy06042003@gmail.com>
2024-11-20 09:41:59 +0700

    câp nhât bai 7

--- while/bai7_lop.py
+++ while/bai7_lop.py
@@ -1,5 +1,7 @@
 n=int(input("Nhập n: "))
 x=0
-while 1<=n<=10 and x<10:
-    x+=1
-    print(n,"*",x,"=",x*n)
+while n<1 or n>10:
+    n=int(input("Nhập n: "))
+    while 1<=n<=10 and x<10:
+        x+=1
+        print(n,"*",x,"=",x*n)
