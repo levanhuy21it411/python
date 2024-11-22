b8f0fcb840851acb368770625a62ab96279f936e
huybmt <levanhuy06042003@gmail.com>
2024-11-22 19:42:57 +0700

    câp nhât mơi

--- vong_lap_for/bai30.py
+++ vong_lap_for/bai30.py
@@ -1,5 +1,6 @@
 a =int(input("Nhập số a: "))
-print("Ước của a là :")
+dem=0
 for i in range(1,a+1):
     if a%i==0:
-        print(i,end=" ")
+        dem=dem+1
+print("Đếm số ước của a là: ",dem)
