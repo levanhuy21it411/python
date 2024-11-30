6dfce4988cc76760b5176a14b045ac80f3f38cf1
huybmt <levanhuy06042003@gmail.com>
2024-11-30 10:05:13 +0700

    cap nhat

--- ham/bai3.py
+++ ham/bai3.py
@@ -2,10 +2,10 @@ a=int(input("Nhập a: "))
 b=int(input("Nhập b: "))
 c=int(input("Nhập c: "))
 def kiemtra(a,b,c):
-    if a>b:
+     if a >= b and a >= c:
         return a
-    if b>c:
+     elif b >= a and b >= c:
         return b
-    else:
+     else:
         return c
 print("kiểm tra số lớn nhất là",kiemtra(a,b,c))
