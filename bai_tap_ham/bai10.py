ad4ee7eccf09791a0c48c213eff0254dbdc912d2
huybmt <levanhuy06042003@gmail.com>
2024-12-11 10:14:33 +0700

    capnhat

--- bai_tap_ham/bai10.py
+++ bai_tap_ham/bai10.py
@@ -1,15 +1,15 @@
 def daoham(n):
     if n == 0:
-        print(1)
+        print (1, end=' ')
     elif n == 1:
-        print(2)
+        print(2, end=' ')
     else:
         f0, f1 = 1, 2 
-        print(f0)
-        print(f1)
+        print(f0, end=' ')
+        print(f1, end=' ')
         for i in range(2, n + 1):
             fn = 2 * f0 + f1 
-            print(fn)
+            print(fn, end=' ')
             f0, f1 = f1, fn
 n = int(input("Nhập giá trị n: "))
 daoham(n)
--- bai_tap_ham/bai11.py
+++ bai_tap_ham/bai11.py
@@ -1,3 +1,3 @@
 n=int(input("Nhập n: "))
 def so_nguyen_to(n):
-    
+    nnnbmn
