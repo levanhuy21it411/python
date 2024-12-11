4ca2e44ff31e339c609b300c838e6b84ccc7916f
huybmt <levanhuy06042003@gmail.com>
2024-12-11 10:28:37 +0700

    cap nhat

--- bai_tap_ham/bai11.py
+++ bai_tap_ham/bai11.py
@@ -1,3 +1,9 @@
 n=int(input("Nhập n: "))
 def so_nguyen_to(n):
-    nnnbmn
+  for i in range(2, n + 1):
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
+        print(i, end=" ")
+so_nguyen_to(n)
